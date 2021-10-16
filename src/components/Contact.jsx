bfac89191d61ec41fe6e6d298babde6c6ebc8647
Darius <dariusford15@gmail.com>
2021-10-16 15:20:55 -0400

    added code to make form a controlled component so that when text is entered into any of the fields the forms state is changed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,31 +1,50 @@
 import React from "react";
+import {useState} from "react";
 
+var initialState = {
+    fname: "",
+    lname: "",
+    email: "",
+    comments: ""
+}
 
 function Contact(){
+    var  [state, setState] = useState(initialState)
+
+    function handleFormChange(e){
+        var newValue = e.target.value;
+        var name= e.target.name;
+
+        setState({
+            ...state,
+            [name]: newValue
+        })
+
+    }
     return (
        <>
         <fieldset>
             <label>First Name</label>
             <br/>
-            <input type="name" name="first-name" placeholder="First Name"/>
+            <input value = {state.fname} type="name" name="fname" placeholder="First Name" onChange={handleFormChange}/>
         </fieldset>
 
         <fieldset>
             <label>Last Name</label>
             <br />
-            <input type="name" name="last-name" placeholder="Last Name"/>
+            <input value = {state.lname} type="name" name="lname" placeholder="Last Name" onChange={handleFormChange}/>
         </fieldset>
 
         <fieldset>
             <label>Email</label>
             <br />
-            <input type="email" placeholer="Email"/>
+            <input value = {state.email} type="email" name="email" placeholer="Email" onChange={handleFormChange}/>
         </fieldset>
 
         <fieldset>
             <label>Comments</label>
             <br/>
-            <textarea name="comments" placeholder="What would you like to say?"/>
+            <textarea value= {state.comments} name="comments" placeholder="What would you like to say?" onChange={handleFormChange} rows="10" cols="60"/>
         </fieldset>
 
         <button>Submit</button>
